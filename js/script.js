f6a92dc11ff61a313217acf7f7b930237ebdc36a
artdimasikk <artemiev.1998@mail.ru>
2023-10-04 15:23:02 +0300

    lesson 16

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
 'use strict';
 
-let taxi={
-    make:'Webville motors',
-    model: "Taxi",
-    year: 1900,
-    color: "yellow",
-    passenger: 4,
-    mileage: 9000,
-    started: true,
-    start: function(){
-        this.started = true;
-    },
-    stop: function(){
-        this.started = false;
-    },
-    drive: function(){
-        if(this.started){
-            console.log('Врум..врум');
-        }else{
-            console.log("Заведи мотор");
-        }
-    }
-};
-taxi.drive();
+let user = ["Makeenko", "Andrew", 28];
+let number = [2,100,25,12,45];
+// let user1 = user.slice(1);
+// console.log(user.length);
+// user.push(180);
+// console.log(user);
+// console.log(user.toString());
+// console.log(user.join(" "));
+// user.pop();
+// console.log(user.pop());
+// user.push(180);
+// console.log(user);
+// user.shift();
+// console.log(user);
+// user.unshift(180);
+// console.log(user);
+// delete user[0];
+// console.log(user);
+// user.splice(2,0,180,8);
+// console.log(user);
+// let family = user.concat(user1, user2, 227);
+// console.log(family);
+// console.log(user1);
+number.sort(function(a,b){
+    return 0.5 - Math.random();
+});
+console.log(number);
